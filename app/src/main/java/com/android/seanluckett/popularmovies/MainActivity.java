5f217bd788206f8c21b2e52a85696ed70b095904
Sean Luckett <whitecrane007@gmail.com>
2019-05-17 15:14:59 -0600

    Adds API key for the movies api.

--- app/src/main/java/com/android/seanluckett/popularmovies/MainActivity.java
+++ app/src/main/java/com/android/seanluckett/popularmovies/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // TODO Move this api key code to api interacter
+    private static final String MOVIE_API_KEY = BuildConfig.MOVIE_API_KEY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

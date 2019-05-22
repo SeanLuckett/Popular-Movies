f6ab22c7179dae18cbed5e217648b5963dc1ab2b
Sean Luckett <whitecrane007@gmail.com>
2019-05-22 09:44:12 -0600

    Makes FilmData parcelable
    In order to pass objects from activity to activity, we need a way to serialize FilmData. Parcelable is the faster way.

--- app/src/main/java/com/android/seanluckett/popularmovies/models/FilmData.java
+++ app/src/main/java/com/android/seanluckett/popularmovies/models/FilmData.java
@@ -1,16 +1,43 @@
 package com.android.seanluckett.popularmovies.models;
 
 import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-import java.net.URL;
+// TODO use parceler library to handle boilerplate some day
+// https://github.com/johncarl81/parceler
 
-public class FilmData {
+public class FilmData implements Parcelable {
 
     private final String mTitle;
     private final Uri mPosterImagePath;
     private final String mPlot;
     private final Double mUserRating;
     private final String mReleaseDate;
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mTitle);
+        dest.writeString(mPlot);
+        dest.writeString(mReleaseDate);
+        dest.writeString(mPosterImagePath.toString());
+
+        dest.writeDouble(mUserRating);
+    }
+
+    private FilmData(Parcel in) {
+        mTitle = in.readString();
+        mPlot = in.readString();
+        mReleaseDate = in.readString();
+        mPosterImagePath = Uri.parse(in.readString());
+
+        mUserRating = in.readDouble();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
     public FilmData(
         String title,
@@ -26,12 +53,12 @@ public class FilmData {
         mReleaseDate = releaseDate;
     }
 
-    public String getTitle() {
-        return mTitle;
-    }
-
     public Uri getPosterImagePath() {
         return mPosterImagePath;
+    }
+
+    public String getTitle() {
+        return mTitle;
     }
 
     public String getPlot() {
@@ -42,7 +69,19 @@ public class FilmData {
         return mUserRating;
     }
 
-    public String getReleaseDate() {
-        return mReleaseDate;
-    }
+    public String getReleaseDate() { return mReleaseDate; }
+
+    public static final Parcelable.Creator<FilmData> CREATOR =
+        new Parcelable.Creator<FilmData>() {
+
+            @Override
+            public FilmData createFromParcel(Parcel source) {
+                return new FilmData(source);
+            }
+
+            @Override
+            public FilmData[] newArray(int size) {
+                return new FilmData[size];
+            }
+        };
 }

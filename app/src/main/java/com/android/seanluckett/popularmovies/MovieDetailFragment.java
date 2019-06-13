5e5244b3cd6ef12525f2bcb7dc93d86dcfc4889a
Sean Luckett <whitecrane007@gmail.com>
2019-06-13 14:13:01 -0600

    Adds viewpager and the first fragment for movie details

--- app/src/main/java/com/android/seanluckett/popularmovies/MovieDetailFragment.java
+++ app/src/main/java/com/android/seanluckett/popularmovies/MovieDetailFragment.java
@@ -20,8 +20,6 @@ import com.android.seanluckett.popularmovies.wrappers.MovieApiWrapper;
  * A simple {@link Fragment} subclass.
  */
 public class MovieDetailFragment extends Fragment {
-    private static final String MOVIE_DETAIL_KEY = MovieDetailPagerAdapter.MOVIE_KEY;
-
     public MovieDetailFragment() {
         // Required empty public constructor
     }
@@ -31,29 +29,24 @@ public class MovieDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        FilmData movie = getArguments().getParcelable(MOVIE_DETAIL_KEY);
+        FilmData movie = getArguments().getParcelable(MovieDetailPagerAdapter.MOVIE_KEY);
 
         View view = inflater.inflate(R.layout.fragment_movie_detail, container, false);
 
         TextView movieTitle = view.findViewById(R.id.movie_title_text);
         movieTitle.setText(movie.getTitle());
-        Log.i("FRAGMENT", "view movie title: " + movieTitle.getText().toString());
 
         ImageView posterView = view.findViewById(R.id.detail_poster_image);
         MovieApiWrapper.loadPosterIntoView(posterView, movie);
 
         TextView rating = view.findViewById(R.id.detail_rating_text);
         rating.setText(movie.getUserRating().toString());
-        Log.i("FRAGMENT", "view movie rating: " + rating.getText().toString());
 
         TextView releaseDate = view.findViewById(R.id.detail_release_date);
         releaseDate.setText(movie.getReleaseYear());
-        Log.i("FRAGMENT", "view movie release date: " + releaseDate.getText().toString());
 
         TextView plot = view.findViewById(R.id.plot_summary);
         plot.setText(movie.getPlot());
-        Log.i("FRAGMENT", "view movie plot: " + plot.getText().toString());
-
 
         return view;
     }
@@ -61,7 +54,7 @@ public class MovieDetailFragment extends Fragment {
     public static MovieDetailFragment newInstance(FilmData movie) {
         
         Bundle args = new Bundle();
-        args.putParcelable(MOVIE_DETAIL_KEY, movie);
+        args.putParcelable(MovieDetailPagerAdapter.MOVIE_KEY, movie);
         
         MovieDetailFragment fragment = new MovieDetailFragment();
         fragment.setArguments(args);

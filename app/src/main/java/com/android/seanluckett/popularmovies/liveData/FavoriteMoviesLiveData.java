01f8b5c0f023fbb7162e513ec8ae364808466ba6
Sean Luckett <whitecrane007@gmail.com>
2019-06-22 15:37:35 -0600

    Adds view model and livedata classes for the list of favorite movies

---
+++ app/src/main/java/com/android/seanluckett/popularmovies/liveData/FavoriteMoviesLiveData.java
@@ -0,0 +1,38 @@
+package com.android.seanluckett.popularmovies.liveData;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import com.android.seanluckett.popularmovies.database.FavoriteDao;
+import com.android.seanluckett.popularmovies.database.FavoriteDatabase;
+import com.android.seanluckett.popularmovies.models.Favorite;
+
+import java.util.List;
+
+public class FavoriteMoviesLiveData extends LiveData {
+    private final FavoriteDao dao;
+
+    public FavoriteMoviesLiveData(Context context) {
+        dao = FavoriteDatabase.getInstance(context).getFavoriteDao();
+        loadData();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void loadData() {
+        new AsyncTask<Void, Void, List<Favorite>>() {
+
+            @Override
+            protected List<Favorite> doInBackground(Void... voids) {
+                return dao.loadFavorites();
+            }
+
+            @Override
+            protected void onPostExecute(List<Favorite> favorites) {
+                setValue(favorites);
+            }
+        }.execute();
+    }
+}
